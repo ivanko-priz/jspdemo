ecdff4232206913e58fea6c2211c3e0df5e3c676
Ivan Prizov <ivanko.priz@gmail.com>
2021-11-05 18:43:26 +0300

    Add javadoc to services

--- src/main/java/com/ivanko/service/api/FoodCategoryService.java
+++ src/main/java/com/ivanko/service/api/FoodCategoryService.java
@@ -7,9 +7,47 @@ import java.sql.SQLException;
 import com.ivanko.model.FoodCategory;
 
 public interface FoodCategoryService {
+    /**
+     * Creates new FoodCategory object
+     *
+     * @param  foodCategory - object be added
+     * @return boolean when success
+     * @throws SQLException
+     */
     boolean create(FoodCategory foodCategory) throws SQLException;
+
+    /**
+     * Updates FoodCategory instance by id
+     *
+     * @param  id - id of item to be updated
+     * @param  foodCategory -  object with updated fields
+     * @return boolean when success
+     * @throws SQLException
+     */
     boolean update(long id, FoodCategory foodCategory) throws SQLException;
+
+    /**
+     * Deletes
+     *
+     * @param id - id of item to be deleted
+     * @return boolean when success
+     * @throws SQLException
+     */
     boolean delete(long id) throws SQLException;
+
+    /**
+     *  Fetches list of FoodCategory of all objects in the table
+     *
+     * @throws SQLException
+     */
     List<FoodCategory> findAll() throws SQLException;
+
+    /**
+     * Finds FoodCategory by id
+     *
+     * @param id - id of item to be fetched
+     * @return Optional with possible item by id
+     * @throws SQLException
+     */
     Optional<FoodCategory> findById(long id) throws SQLException;
 }
--- src/main/java/com/ivanko/service/api/FoodService.java
+++ src/main/java/com/ivanko/service/api/FoodService.java
@@ -8,9 +8,50 @@ import com.ivanko.exception.BusinessRuleException;
 import com.ivanko.model.Food;
 
 public interface FoodService {
+    /**
+     *  Creates food
+     *
+     * @param food - food item to be created
+     * @return true when success
+     * @throws SQLException
+     * @throws BusinessRuleException if negative calories are provided
+     */
     boolean create(Food food) throws SQLException, BusinessRuleException;
+
+    /**
+     * Updates food based on id with provided Food object
+     *
+     * @param id - id of item to be updated
+     * @param food - new fields which will be set
+     * @return true if success
+     * @throws SQLException
+     * @throws BusinessRuleException if negative calories are provided
+     */
     boolean update(long id, Food food) throws SQLException, BusinessRuleException;
+
+    /**
+     *  Deletes food by id
+     *
+     * @param id - id of item to be deleted
+     * @return true if success
+     * @throws SQLException
+     */
     boolean delete(long id) throws SQLException;
+
+    /**
+     * Fetches all Food objects from the database
+     *
+     * @return List of Food
+     * @throws SQLException
+     */
     List<Food> findAll() throws SQLException;
+
+    /**
+     * Fetches by Food object by id
+     *
+     * @param id - id of item to be fetched
+     * @return Optional with Food inside if food with such id exists
+     * @throws SQLException
+     */
     Optional<Food> findById(long id) throws SQLException;
 }
