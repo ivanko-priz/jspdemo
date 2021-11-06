d7f36c220b12f89199315075fdd661b97cc8619e
Ivan Prizov <ivanko.priz@gamil.com>
2021-11-06 16:31:58 +0300

    Add tests for Food Category service

--- src/test/java/com/ivanko/service/impl/FoodCategoryServiceImplTest.java
+++ src/test/java/com/ivanko/service/impl/FoodCategoryServiceImplTest.java
@@ -1,17 +1,21 @@
 package com.ivanko.service.impl;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import static org.mockito.Mockito.*;
+
+import java.util.List;
+import java.util.Optional;
 
 import com.ivanko.dao.api.Dao;
 import com.ivanko.model.FoodCategory;
 import com.ivanko.service.api.FoodCategoryService;
 
-import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class FoodCategoryServiceImplTest {
@@ -31,5 +35,47 @@ public class FoodCategoryServiceImplTest {
         foodCategoryService.create(foodCategory);
 
         verify(foodCategoryDaoMock, times(1)).create(foodCategory);
+    }
+
+    @Test
+    public void shouldUpdateFoodCategoryUsingDao() throws Exception {
+        when(foodCategoryDaoMock.update(anyLong(), any(FoodCategory.class))).thenReturn(true);
+
+        FoodCategory foodCategory = new FoodCategory();
+        long id = 10L;
+
+        foodCategoryService.update(id, foodCategory);
+
+        verify(foodCategoryDaoMock, times(1)).update(id, foodCategory);
+    }
+
+    @Test
+    public void shouldDeleteFoodCategoryUsingDao() throws Exception {
+        when(foodCategoryDaoMock.delete(anyLong())).thenReturn(true);
+        long id = 10L;
+
+        foodCategoryService.delete(id);
+
+        verify(foodCategoryDaoMock, times(1)).delete(id);
+    }
+
+    @Test
+    public void shouldFindByIdUsingDao() throws Exception {
+        when(foodCategoryDaoMock.findById(anyLong())).thenReturn(Optional.of(new FoodCategory()));
+
+        long id = 10L;
+
+        foodCategoryService.findById(id);
+        verify(foodCategoryDaoMock, times(1)).findById(id);
+    }
+
+    @Test
+    public void shouldFindAllUsingDao() throws Exception {
+        when(foodCategoryDaoMock.findAll()).thenReturn(List.of(new FoodCategory()));
+
+        List<FoodCategory> foodCategories = foodCategoryService.findAll();
+
+        verify(foodCategoryDaoMock, times(1)).findAll();
+        Assertions.assertEquals(1, foodCategories.size());
     }
 }
