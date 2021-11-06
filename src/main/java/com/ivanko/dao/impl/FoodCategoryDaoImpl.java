7d9e59780159ccb9c3f28f4ce84860d40cf7874b
Ivan Prizov <ivanko.priz@gamil.com>
2021-11-06 21:58:56 +0300

    Wrap jdbc connection in try-with-resources

--- src/main/java/com/ivanko/dao/impl/FoodCategoryDaoImpl.java
+++ src/main/java/com/ivanko/dao/impl/FoodCategoryDaoImpl.java
@@ -22,68 +22,90 @@ public class FoodCategoryDaoImpl implements Dao<FoodCategory, Long> {
     @Override
     public boolean create(FoodCategory obj) throws SQLException {
         final String sql = "INSERT INTO food_category (name) VALUES (?)";
-        Connection conn = datasourceFactory.getConnection();
-        PreparedStatement statement = conn.prepareStatement(sql);
-        statement.setString(1, obj.getName());
 
-        return statement.execute();
+        try (Connection conn = datasourceFactory.getConnection()) {
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setString(1, obj.getName());
+
+            return statement.execute();
+        } catch (SQLException e) {
+            throw e;
+        }
     }
 
     @Override
     public boolean update(Long aLong, FoodCategory obj) throws SQLException {
         final String sql = "UPDATE food_category SET name = ? WHERE id = ?";
-        Connection conn = datasourceFactory.getConnection();
-        PreparedStatement statement = conn.prepareStatement(sql);
-        statement.setString(1, obj.getName());
-        statement.setLong(2, aLong);
 
-        return statement.executeUpdate() == 1;    }
+        try (Connection conn = datasourceFactory.getConnection()) {
+            PreparedStatement statement = conn.prepareStatement(sql);
+
+            statement.setString(1, obj.getName());
+            statement.setLong(2, aLong);
+
+            return statement.executeUpdate() == 1;
+        } catch (SQLException e) {
+            throw e;
+        }
+   }
 
     @Override
     public boolean delete(Long aLong) throws SQLException {
         final String sql = "DELETE FROM food_category WHERE id = ?";
-        Connection conn = datasourceFactory.getConnection();
-        PreparedStatement statement = conn.prepareStatement(sql);
-        statement.setLong(1, aLong);
 
-        return statement.execute();
+        try (Connection conn = datasourceFactory.getConnection()) {
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setLong(1, aLong);
+
+            return statement.execute();
+        } catch (SQLException e) {
+            throw e;
+        }
     }
 
     @Override
     public Optional<FoodCategory> findById(Long aLong) throws SQLException {
         final String sql = "SELECT id, name, created_at FROM food_category WHERE id = ?";
-        Connection conn = datasourceFactory.getConnection();
-        PreparedStatement statement = conn.prepareStatement(sql);
-        statement.setLong(1, aLong);
 
-        ResultSet resultSet = statement.executeQuery();
-        Optional<FoodCategory> result;
+        try (Connection conn = datasourceFactory.getConnection()) {
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setLong(1, aLong);
 
-        if (resultSet.next()) {
-            FoodCategory foodCategory = SqlResultToFoodCategoryMapper.map(resultSet);
-            result = Optional.of(foodCategory);
-        } else {
-            result = Optional.empty();
+            ResultSet resultSet = statement.executeQuery();
+            Optional<FoodCategory> result;
+
+            if (resultSet.next()) {
+                FoodCategory foodCategory = SqlResultToFoodCategoryMapper.map(resultSet);
+                result = Optional.of(foodCategory);
+            } else {
+                result = Optional.empty();
+            }
+
+            return result;
+        } catch (SQLException e) {
+            throw e;
         }
-
-        return result;
     }
 
     @Override
     public List<FoodCategory> findAll() throws SQLException {
         final String sql = "SELECT * FROM food_category";
-        Connection conn = datasourceFactory.getConnection();
-        Statement statement = conn.createStatement();
 
-        ResultSet resultSet = statement.executeQuery(sql);
-        ArrayList<FoodCategory> result = new ArrayList<>(resultSet.getFetchSize());
+        try (Connection conn = datasourceFactory.getConnection()) {
+            Statement statement = conn.createStatement();
 
-        while(resultSet.next()) {
-            FoodCategory foodCategory = SqlResultToFoodCategoryMapper.map(resultSet);
-            result.add(foodCategory);
+            ResultSet resultSet = statement.executeQuery(sql);
+            ArrayList<FoodCategory> result = new ArrayList<>(resultSet.getFetchSize());
+
+            while(resultSet.next()) {
+                FoodCategory foodCategory = SqlResultToFoodCategoryMapper.map(resultSet);
+                result.add(foodCategory);
+            }
+
+            return result;
+        } catch (SQLException e) {
+            throw e;
         }
-
-        return result;
     }
 
     @Override
--- src/main/java/com/ivanko/dao/impl/FoodDaoImpl.java
+++ src/main/java/com/ivanko/dao/impl/FoodDaoImpl.java
@@ -25,107 +25,130 @@ public class FoodDaoImpl implements Dao<Food, Long> {
     @Override
     public boolean create(Food obj) throws SQLException {
         final String sql = "INSERT INTO food (name, food_category_id, description, calories_per_100_g) VALUES (?, ?, ?, ?)";
-        Connection conn = datasourceFactory.getConnection();
 
-        PreparedStatement statement;
-        statement = conn.prepareStatement(sql);
+        try (Connection conn = datasourceFactory.getConnection()) {
+            PreparedStatement statement;
+            statement = conn.prepareStatement(sql);
 
-        statement.setString(1, obj.getName());
+            statement.setString(1, obj.getName());
 
-        if (obj.getFoodCategoryId() == 0) {
-            statement.setNull(2, Types.BIGINT);
-        } else {
-            statement.setLong(2, obj.getFoodCategoryId());
+            if (obj.getFoodCategoryId() == 0) {
+                statement.setNull(2, Types.BIGINT);
+            } else {
+                statement.setLong(2, obj.getFoodCategoryId());
+            }
+
+            statement.setString(3, obj.getDescription());
+            statement.setInt(4, obj.getCaloriesPer100g());
+
+            return statement.execute();
+        } catch (SQLException e) {
+            throw e;
         }
-
-        statement.setString(3, obj.getDescription());
-        statement.setInt(4, obj.getCaloriesPer100g());
-
-        return statement.execute();
     }
 
     @Override
     public boolean update(Long aLong, Food obj) throws SQLException {
         final String sql = "UPDATE food SET name = ?, food_category_id = ?, description = ?, calories_per_100_g = ? WHERE id = ?";
-        Connection conn = datasourceFactory.getConnection();
-        PreparedStatement statement = conn.prepareStatement(sql);
 
-        statement.setString(1, obj.getName());
-        if (obj.getFoodCategoryId() == 0) {
-            statement.setNull(2, Types.BIGINT);
-        } else {
-            statement.setLong(2, obj.getFoodCategoryId());
+        try (Connection conn = datasourceFactory.getConnection()) {
+            PreparedStatement statement = conn.prepareStatement(sql);
+
+            statement.setString(1, obj.getName());
+            if (obj.getFoodCategoryId() == 0) {
+                statement.setNull(2, Types.BIGINT);
+            } else {
+                statement.setLong(2, obj.getFoodCategoryId());
+            }
+
+            statement.setString(3, obj.getDescription());
+            statement.setInt(4, obj.getCaloriesPer100g());
+
+            statement.setLong(5, aLong);
+
+            return statement.executeUpdate() == 1;
+        } catch (SQLException e) {
+            throw e;
         }
-
-        statement.setString(3, obj.getDescription());
-        statement.setInt(4, obj.getCaloriesPer100g());
-
-        statement.setLong(5, aLong);
-
-        return statement.executeUpdate() == 1;
     }
 
     @Override
     public boolean delete(Long aLong) throws SQLException {
         final String sql = "DELETE FROM food WHERE id = ?";
-        Connection conn = datasourceFactory.getConnection();
-        PreparedStatement statement = conn.prepareStatement(sql);
-        statement.setLong(1, aLong);
 
-        return statement.execute();
+        try (Connection conn = datasourceFactory.getConnection()) {
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setLong(1, aLong);
+
+            return statement.execute();
+        } catch (SQLException e) {
+            throw e;
+        }
     }
 
     @Override
     public Optional<Food> findById(Long aLong) throws SQLException {
         final String sql = "SELECT id, name, food_category_id, description, calories_per_100_g, created_at FROM food WHERE id = ?";
-        Connection conn = datasourceFactory.getConnection();
-        PreparedStatement statement = conn.prepareStatement(sql);
-        statement.setLong(1, aLong);
 
-        ResultSet resultSet = statement.executeQuery();
-        Optional<Food> result;
+        try (Connection conn = datasourceFactory.getConnection()) {
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setLong(1, aLong);
 
-        if (resultSet.next()) {
-            Food food = SqlResultToFoodMapper.mapToFood(resultSet);
-            result = Optional.of(food);
-        } else {
-            result = Optional.empty();
+            ResultSet resultSet = statement.executeQuery();
+            Optional<Food> result;
+
+            if (resultSet.next()) {
+                Food food = SqlResultToFoodMapper.mapToFood(resultSet);
+                result = Optional.of(food);
+            } else {
+                result = Optional.empty();
+            }
+
+            return result;
+        } catch (SQLException e) {
+            throw e;
         }
-
-        return result;
     }
 
     @Override
     public List<Food> findAll() throws SQLException {
         final String sql = "SELECT * FROM food";
-        Connection conn = datasourceFactory.getConnection();
-        Statement statement = conn.createStatement();
 
-        ResultSet resultSet = statement.executeQuery(sql);
-        ArrayList<Food> result = new ArrayList<>(resultSet.getFetchSize());
+        try (Connection conn = datasourceFactory.getConnection()) {
+            Statement statement = conn.createStatement();
 
-        while(resultSet.next()) {
-            Food food = SqlResultToFoodMapper.mapToFood(resultSet);
-            result.add(food);
+            ResultSet resultSet = statement.executeQuery(sql);
+            ArrayList<Food> result = new ArrayList<>(resultSet.getFetchSize());
+
+            while(resultSet.next()) {
+                Food food = SqlResultToFoodMapper.mapToFood(resultSet);
+                result.add(food);
+            }
+
+            return result;
+        } catch (SQLException e) {
+            throw e;
         }
-
-        return result;
     }
 
     @Override
     public List<FoodWrapper> findJoinedAll() throws SQLException {
         final String sql = "SELECT f.id AS id, f.name AS name, f_c.name AS category, f.food_category_id as food_category_id, f.description AS description, f.calories_per_100_g AS calories_per_100_g, f.created_at AS created_at FROM food f LEFT JOIN food_category f_c ON f_c.id = f.food_category_id";
-        Connection conn = datasourceFactory.getConnection();
-        Statement statement = conn.createStatement();
 
-        ResultSet resultSet = statement.executeQuery(sql);
-        ArrayList<FoodWrapper> result = new ArrayList<>(resultSet.getFetchSize());
+        try (Connection conn = datasourceFactory.getConnection()) {
+            Statement statement = conn.createStatement();
 
-        while(resultSet.next()) {
-            FoodWrapper food = SqlResultToFoodMapper.mapToWrapper(resultSet);
-            result.add(food);
+            ResultSet resultSet = statement.executeQuery(sql);
+            ArrayList<FoodWrapper> result = new ArrayList<>(resultSet.getFetchSize());
+
+            while(resultSet.next()) {
+                FoodWrapper food = SqlResultToFoodMapper.mapToWrapper(resultSet);
+                result.add(food);
+            }
+
+            return result;
+        } catch (SQLException e) {
+            throw e;
         }
-
-        return result;
     }
 }
